b5a590fddbe0c9764c4aeaf09e976bfd130b1731
nathan-rr-mello <nathanreismello2@gmail.com>
2025-04-10 18:10:51 -0300

    code

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int contar_somas_consecutivas(int n) {
+    int count = 0;
+    for (int k = 1; k * (k + 1) / 2 <= n; k++) {
+        int numerador = n - k * (k - 1) / 2;
+        if (numerador % k == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+void encontrar_maior_representacao(int limite) {
+    int max_count = 0;
+    int numero_com_mais_formas = 0;
+
+    for (int i = 1; i <= limite; i++) {
+        int formas = contar_somas_consecutivas(i);
+        if (formas > max_count) {
+            max_count = formas;
+            numero_com_mais_formas = i;
+        }
+    }
+
+    printf("Número com mais representações entre 1 e %d: %d\n", limite, numero_com_mais_formas);
+    printf("Total de formas: %d\n", max_count);
+}
+
+int main() {
+    int limite = 10000000; //10_000_000
+    encontrar_maior_representacao(limite);
+    return 0;
+}
