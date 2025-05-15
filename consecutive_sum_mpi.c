28afc76a88d4d377ada75e9f54f82d37d6c2b6ad
Camila <83407255+camilailges@users.noreply.github.com>
2025-05-15 18:59:09 -0300

    Update consecutive_sum_mpi.c

--- consecutive_sum_mpi.c
+++ consecutive_sum_mpi.c
@@ -39,18 +39,18 @@ Resultado encontrar_maior_representacao_trivial(int inicio, int fim) {
     // printf("Total de formas: %d\n", maior_resultado.somas);
     return maior_resultado;
 }
-
 void separa_trabalho(int proc_n, int limite) {
 
     int tag = 50;
-    for (int i = 0; i < proc_n; i++) {
+    for (int i = 1; i < proc_n; i++) {
+
         int inicio = (limite / proc_n) * i + 1;
         int fim = (limite / proc_n) * (i + 1);
         if (i == proc_n - 1) {
             fim = limite;
         }
         int range[2] = {inicio, fim};
-        MPI_Send(range, 2, MPI_INT, i + 1, tag, MPI_COMM_WORLD);
+        MPI_Send(range, 2, MPI_INT, i, tag, MPI_COMM_WORLD);
     }
 }
 
@@ -74,23 +74,27 @@ void finaliza_trabalho(int proc_n, int limite) {
     Resultado maior_resultado = {0, 0};
     int tag = 50;
     MPI_Status status; // estrutura que guarda o estado de retorno
-    for (int i = 1; i <= proc_n; i++) {
+    for (int i = 1; i < proc_n; i++) {
         int resposta[2];
         MPI_Recv(resposta, 2, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
         Resultado resultado = {resposta[0], resposta[1]};
         if (resultado.somas > maior_resultado.somas) {
             maior_resultado = resultado;
-        }
+		}
     }
 
     printf("Número com mais representações entre 1 e %d: %d\n", limite, maior_resultado.numero);
     printf("Total de formas: %d\n", maior_resultado.somas);
 }
 
+void pede_trabalho() {
+   MPI_Send(maior_resultado, 2, MPI_INT, source, tag, MPI_COMM_WORLD);
+}
+
 int main(int argc, char** argv)
 {
-    int limite = 1000; // 25_000_000
-    // int limite = 9;
+    int limite = 1000000; // 25_000_000
+    //int limite = 9;
     int my_rank;       // Identificador deste processo
     int proc_n;        // Numero de processos disparados pelo usuario na linha de comando (np)
     int message;       // Buffer para as mensagens
@@ -98,12 +102,17 @@ int main(int argc, char** argv)
 
     MPI_Init(&argc, &argv); // funcao que inicializa o MPI, todo o codigo paralelo estah abaixo
 
+    double t1,t2;
+    t1 = MPI_Wtime();  // inicia a contagem do tempo
+
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // pega pega o numero do processo atual (rank)
     MPI_Comm_size(MPI_COMM_WORLD, &proc_n);  // pega informacao do numero de processos (quantidade total)
 
     if (my_rank == 0) {
         separa_trabalho(proc_n, limite);
         finaliza_trabalho(proc_n, limite);
+        t2 = MPI_Wtime(); // termina a contagem do tempo
+        printf("\nTempo de execucao: %f\n\n", t2-t1);
     } else {
         realiza_trabalho();
     }
