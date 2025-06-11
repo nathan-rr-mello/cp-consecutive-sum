f7adc34459e876ec03a2c57d8713db64c321fdec
nathan-rr-mello <nathanreismello2@gmail.com>
2025-06-11 18:53:48 -0300

    div e conquista

--- divisao_e_conquista/divisao_e_conquista.c
+++ divisao_e_conquista/divisao_e_conquista.c
@@ -1,6 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 
-#define VETOR_SIZE 1000
+#define VETOR_SIZE 40
+
+int* inicializa(int tam_vetor) {
+    int* vetor = (int*) malloc(tam_vetor * sizeof(int));
+    for (int i = 0; i < tam_vetor; i++) {
+        vetor[i] = tam_vetor - i; // ordem reversa
+    }
+    return vetor;
+}
+
+void bubblesort(int* vetor, int tam_vetor) {
+    for (int i = 0; i < tam_vetor - 1; i++) {
+        for (int j = 0; j < tam_vetor - i - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                // troca
+                int temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void intercala(int** vetor, int tam_vetor) {
+    int i = 0, j = tam_vetor/2, k = 0;
+    int tam_esquerda = tam_vetor / 2; // tamanho da metade esquerda
+    int* result = (int*) malloc(tam_vetor * sizeof(int)); // vetor para armazenar o resultado
+
+    // Enquanto houver elementos em ambos os arrays
+    while (i < tam_esquerda && j < tam_vetor) {
+        if ((*vetor)[i] <= (*vetor)[j]) {
+            result[k++] = (*vetor)[i++];
+        } else {
+            result[k++] = (*vetor)[j++];
+        }
+    }
+
+    // Copia o restante do array A (se sobrou algo)
+    while (i < tam_esquerda) {
+        result[k++] = (*vetor)[i++];
+    }
+
+    // Copia o restante do array B (se sobrou algo)
+    while (j < tam_vetor) {
+        result[k++] = (*vetor)[j++];
+    }
+
+    *vetor = result;
+}
+
+void mostra(int* vetor) {
+    printf("[");
+    for (int i = 0; i < VETOR_SIZE; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("]\n");
+}
 
 int main(int argc, char **argv) {
     int my_rank;
@@ -9,7 +67,7 @@ int main(int argc, char **argv) {
 
     int delta = 10;  // tamanho do vetor para ordenar diretamente
     int* vetor;
-    int tam_vetor;
+    int tam_vetor = VETOR_SIZE; // defino tamanho inicial do vetor
     int pai;
 
     if (my_rank != 0) {
@@ -17,10 +75,12 @@ int main(int argc, char **argv) {
         MPI_Status status;
         MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         MPI_Get_count(&status, MPI_INT, &tam_vetor); // descubro tamanho da mensagem recebida
-        MPI_Recv(&vetor, tam_vetor, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status); // não sou a raiz, tenho pai
+        vetor = (int*) malloc(tam_vetor * sizeof(int)); // aloco vetor com tamanho recebido
+        pai = status.MPI_SOURCE;
+        MPI_Recv(&vetor[0], tam_vetor, MPI_INT, pai, MPI_ANY_TAG, MPI_COMM_WORLD, &status); // não sou a raiz, tenho pai
     } else {
-        tam_vetor = VETOR_SIZE;               // defino tamanho inicial do vetor
-        inicializa(vetor, tam_vetor);      // sou a raiz e portanto gero o vetor - ordem reversa
+        // inicializa(&vetor, tam_vetor);      // sou a raiz e portanto gero o vetor - ordem reversa
+        vetor = inicializa(tam_vetor);
     }
 
     // dividir ou conquistar?
@@ -42,7 +102,7 @@ int main(int argc, char **argv) {
         MPI_Recv(&vetor[tam_vetor/2], tam_direita, MPI_INT, filho_direita, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         // intercalo vetor inteiro
-        intercala(vetor, tam_vetor);
+        intercala(&vetor, tam_vetor);
     }
 
     // mando para o pai
@@ -54,60 +114,3 @@ int main(int argc, char **argv) {
 
     MPI_Finalize();
 }
-
-void inicializa(int* vetor, int tam_vetor) {
-    vetor = (int*) malloc(tam_vetor * sizeof(int));
-    for (int i = 0; i < tam_vetor; i++) {
-        vetor[i] = tam_vetor - i; // ordem reversa
-    }
-}
-
-void bubblesort(int* vetor, int tam_vetor) {
-    for (int i = 0; i < tam_vetor - 1; i++) {
-        for (int j = 0; j < tam_vetor - i - 1; j++) {
-            if (vetor[j] > vetor[j + 1]) {
-                // troca
-                int temp = vetor[j];
-                vetor[j] = vetor[j + 1];
-                vetor[j + 1] = temp;
-            }
-        }
-    }
-}
-
-void intercala(int* vetor, int tam_vetor) {
-    int i = 0, j = tam_vetor/2, k = 0;
-    int tam_esquerda = tam_vetor / 2; // tamanho da metade esquerda
-    int tam_direita = tam_vetor - tam_esquerda; // tamanho da metade direita
-    int* result = (int*) malloc(tam_vetor * sizeof(int)); // vetor para armazenar o resultado
-
-    // Enquanto houver elementos em ambos os arrays
-    while (i < tam_esquerda && j < tam_direita) {
-        if (vetor[i] <= vetor[j]) {
-            result[k++] = vetor[i++];
-        } else {
-            result[k++] = vetor[j++];
-        }
-    }
-
-    // Copia o restante do array A (se sobrou algo)
-    while (i < tam_esquerda) {
-        result[k++] = vetor[i++];
-    }
-
-    // Copia o restante do array B (se sobrou algo)
-    while (j < tam_direita) {
-        result[k++] = vetor[j++];
-    }
-
-    vetor = result;
-}
-
-void mostra(int* vetor) {
-    printf("[");
-    for (int i = 0; i < VETOR_SIZE; i++) {
-        printf("%d ", vetor[i]);
-    }
-    printf("]\n");
-}
-
